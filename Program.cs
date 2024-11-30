a6a6ef10dfec8ff4b42e0e948c86dea6b3003328
Remenod <remenod@gmail.com>
2024-11-30 18:20:44 +0200

    task start selectors update

--- Program.cs
+++ Program.cs
@@ -2,7 +2,7 @@
 using static System.ConsoleColor;
 using AMLR = Lab4.ArrayMethodLessRealization;
 using AMFR = Lab4.ArrayMethodFulRealization;
-using AUR = Lab4.ArrayUniversalRealization;
+using ALR = Lab4.ArrayLessRealization;
 
 namespace Lab4
 {
@@ -67,12 +67,34 @@ namespace Lab4
     {
 
     }
-    static class ArrayUniversalRealization 
+    static class ArrayLessRealization 
     {
-        static public bool SelectArrayMethodsUsage() 
-        {
-            Custom.WriteColored("Виберіть тип виконання програми.\n", White, "0",Yellow," - Без методів классу Array стандартої бібліотеки.\n",White,"1",Yellow," - З методами классу Array стандартої бібліотеки.\n", White);
-            return Convert.ToBoolean(Custom.ReadLine(Convert.ToInt32, true, "Неправильний тип введення",Red , White, true));
+
+        static public byte SelectInputType() 
+        {            
+            Custom.WriteColored("Виберіть тип введення:\n", White, "0", Yellow, " - заповнити масив випадковими числами.\n", White, "1", Yellow, " - заповнити масив числами в рядок через пробіл.\n", White, "2", Yellow, " - заповнити масив в стовпчик через ", White, "Enter\n", Yellow);
+            start:
+            var x = Custom.ReadLine(Convert.ToByte, true, "Неправильний тип введення", Red, White, true);
+            if (x == 0 || x == 1 || x == 2)
+                return x;
+            else
+            {
+                Custom.WriteColored("Неправильний тип введення\n", Red);
+                goto start;
+            }
+        }
+        static public byte SelectArrayMethodsUsage() 
+        {            
+            Custom.WriteColored("Виберіть тип виконання програми:\n", White, "0",Yellow," - Без методів классу Array стандартої бібліотеки.\n",White,"1",Yellow," - З методами классу Array стандартої бібліотеки.\n", White);
+            start:
+            var x = Custom.ReadLine(Convert.ToByte, true, "Неправильний тип введення",Red , White, true);
+            if (x == 0 || x == 1)
+                return x;
+            else
+            {
+                Custom.WriteColored("Неправильний тип введення\n", Red);
+                goto start;
+            }
         }
     }    
     internal class Program
@@ -80,7 +102,9 @@ namespace Lab4
         static void Main()
         {
             Console.OutputEncoding = System.Text.Encoding.Unicode;
-            var isArrayMethodsUse = AUR.SelectArrayMethodsUsage();
+            ALR.SelectArrayMethodsUsage();
+            ALR.SelectInputType();
+            
             
         }
     }
